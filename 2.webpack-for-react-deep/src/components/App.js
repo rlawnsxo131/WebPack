11550531ceb6c360ba541097ebea33769f96079c
juntae <rlawnsxo131@gmail.com>
2019-01-22 23:45:37 +0900

    20190122

--- 2.webpack-for-react-deep/src/components/App.js
+++ 2.webpack-for-react-deep/src/components/App.js
@@ -5,12 +5,13 @@ import importedComponent from 'react-imported-component';
 import Home from './Home';
 import Loading from './Loading';
 
-const AsyncDynamicPAge = importedComponent(
+const AsyncDynamicPage = importedComponent(
   () => import(/* webpackChunkName:'DynamicPage' */ './DynamicPage'),
   {
     LoadingComponent: Loading
   }
 );
+
 const AsyncNoMatch = importedComponent(
   () => import(/* webpackChunkName:'NoMatch' */ './NoMatch'),
   {
@@ -24,7 +25,7 @@ const App = () => {
       <div>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/dynamic" component={AsyncDynamicPAge} />
+          <Route exact path="/dynamic" component={AsyncDynamicPage} />
           <Route component={AsyncNoMatch} />
         </Switch>
       </div>
